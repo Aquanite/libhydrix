5bc74cbca32199f8c9b9254ba20538751aea8801
Azure <alexmybestcat@gmail.com>
2024-07-02 01:41:38 -0400

    Update FPU
    Fixed to C++ Standards

--- src/hlow/fpu/fpu.cpp
+++ src/hlow/fpu/fpu.cpp
@@ -1,23 +1,23 @@
 #include "fpu.h"
 #include "cpuid.h"
-bool __fpu_enabled__ = false;
+bool fpu_enabled = false;
 void DoesFPUExist()
 {
     uint32_t eax, ebx, ecx, edx;
     __get_cpuid(0x00000001, &eax, &ebx, &ecx, &edx);
     if (edx & (1 << 0))
     {
-        __fpu_enabled__ = true;
+        fpu_enabled = true;
     }
     else
     {
-        __fpu_enabled__ = false;
+        fpu_enabled = false;
     }
 }
 void FPU::Enable()
 {
     DoesFPUExist();
-    if (__fpu_enabled__)
+    if (fpu_enabled)
     {
         __asm__ __volatile__("fninit");
     }
@@ -29,7 +29,7 @@ void FPU::Enable()
 }
 bool FPU::Is_Enabled()
 {
-    return __fpu_enabled__;
+    return fpu_enabled;
 }
 
 void FPU::Init()
